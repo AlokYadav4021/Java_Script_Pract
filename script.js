2c0089a4f7b2ffac30b25e8e993acd339e00b893
Alok Yadav <alokyadav4021@gmail.com>
2026-09-17 22:40:37 +0530

    Variable questions are done

---
+++ script.js
@@ -0,0 +1,95 @@
+// JavaScript Variables
+// Question 1
+
+var name = "Alok Yadav"
+console.log(name)
+
+// Question 2
+
+var name = "Alok Yadav";
+var age = 21;
+var city = "Mumbai"
+console.log("My name is " + (name) + ", I'm " + (age) + " years old and i live in "+(city))
+
+// Question 3
+
+var length = 20;
+var width = 30;
+
+console.log(length * width)
+
+
+// Question 4
+
+var a = 20
+var b = 35
+console.log(a+b)
+console.log(a - b)
+console.log(a*b)
+console.log(a/b)
+
+
+// Question 5
+
+var birthYear = 2004;
+var currentYear = 2026;
+
+console.log((currentYear) - (birthYear))
+
+// Question 6
+
+var firstName = "Alok"
+var lastName = "Yadav"
+
+var fullName = ((firstName) +" "+ (lastName))
+
+console.log(fullName)
+
+// Question 7
+
+var prodPrice = 100;
+var prodQunt = 1000;
+
+var totalPrice = (prodPrice * prodQunt)
+console.log(totalPrice)
+
+// Question 8
+
+var mark1 = 20
+var mark2 = 30
+var mark3 = 50
+
+var totalMark = ((mark1)+(mark2) + (mark3))
+var avg = (((mark1)+(mark2) + (mark3)) / 3)
+
+console.log("Total Marks of the Student is " + (totalMark) + " And Average is " +(avg))
+
+
+// Question 9
+
+var temp = 35
+
+console.log("Temp in ferenhite is " + ((temp * 9 / 5) + 32))
+
+
+
+// Question 10
+
+var rupee = 830000;
+
+var doller = 83;
+
+console.log((rupee) / (doller))
+
+
+// Question 11
+
+var number = 85;
+number += 10 ;
+console.log(number)
+
+// Question 12
+
+var score = 100;
+score -= 25;
+console.log(score)
