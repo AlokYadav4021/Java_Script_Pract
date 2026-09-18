1feee13c8ce6d1ecbb9e9338c8ef90f4cdd5f4d2
Alok Yadav <alokyadav4021@gmail.com>
2026-09-18 23:44:57 +0530

    conditional statements Beginner practice

---
+++ conditional_statement.js
@@ -0,0 +1,71 @@
+//Conditional Statement
+
+//Problam 1
+
+// var number = prompt("Enter a Number")
+
+// if (number >= 1){
+//     console.log("Number is Positive")
+// }
+// else{
+//     console.log("Number is Negative")
+// }
+
+
+
+
+//Problam 2
+
+
+// var even = prompt("Enter Another Number")
+
+// if(even % 2 === 0 ){
+//     console.log("Number is even")
+// }
+// else{
+//     console.log("Number is odd");
+
+// }
+
+
+
+//Problam 3
+
+
+
+// var age = prompt("Enter Your Age")
+// if (age >= 18){
+//     console.log("Eligible to vote");
+// }
+// else{
+//     console.log("Not eligible to vote");
+// }
+
+
+//Problam 4
+
+
+
+// var age = prompt("Enter Your Age")
+// if(age > 18){
+//     console.log("Enjoy the Party Sir")
+// }
+// else{
+//     console.log("Kid go and Drink Cola")
+// }
+
+
+
+//Problam 5
+
+
+var num1 = prompt("enter Number 1")
+var num2 = prompt("enter Number 2")
+
+if (num1 > num2){
+    console.log("Number 1 is bigger")
+}else if (num1 < num2) {
+  console.log("Number 2 is bigger");
+}else{
+    console.log("Both Numbers are Equal")
+}
