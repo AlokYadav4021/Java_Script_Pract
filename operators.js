2a125342e70552577a7a7dff468247d09931b08b
Alok Yadav <alokyadav4021@gmail.com>
2026-09-18 21:55:57 +0530

    I perform Operators

---
+++ operators.js
@@ -0,0 +1,74 @@
+//operators
+
+//Problam 1.
+
+var a = 20;
+var b = 5;
+
+console.log(a + b);
+
+//Problam 2.
+
+var a1 = 50;
+var b1 = 15;
+
+console.log(b1 - a1);
+
+//Problam 3
+
+var a2 = 12;
+var b2 = 4;
+
+console.log(a2 / b2);
+
+//Problam 4
+
+var score = 100;
+score += 20;
+console.log(score);
+
+//Problam 5
+
+var dicr = 500;
+dicr -= 100
+console.log(dicr)
+
+//Problam 6
+
+var mul = 10
+mul *= 5
+console.log(mul)
+
+//Problam 7
+
+var balance =  1000
+balance /= 4
+console.log(balance)
+
+
+//Problam 8
+
+var count = 25;
+count %= 4
+console.log(count)
+
+
+//Problam 9
+
+
+var likes =  10;
+likes ++
+console.log(likes)
+
+//Problam 10
+
+var dislike = 3;
+dislike --
+console.log(dislike)
+
+//Problam 11
+
+var x = 10;
+x ++
+x ++
+console.log(x) 
