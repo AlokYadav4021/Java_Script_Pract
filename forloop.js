52b8ea0533ff492900ad13885f3843dcf4eaee80
Alok Yadav <alokyadav4021@gmail.com>
2026-09-24 00:00:07 +0530

    For loop Practice begeners

---
+++ forloop.js
@@ -0,0 +1,22 @@
+// 1. Print numbers from 1 to 10
+// Use a for loop to print numbers from 1 to 10.
+
+for(let i=1; i<=10;i++){
+    console.log(i)
+}
+
+// 2. Print numbers from 10 to 1
+// Use a for loop to print numbers in reverse order.
+
+for (let i =10 ; i >= 1; i--){
+    console.log(i)
+}
+
+// 3.Print even numbers
+// Print all even numbers between 1 and 20
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
