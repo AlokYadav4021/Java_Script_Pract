7c95a649aa965162c062878a4c08d48cad24af17
Alok Yadav <alokyadav4021@gmail.com>
2026-09-24 00:48:02 +0530

    some more practice questions of for loop

--- forloop.js
+++ forloop.js
@@ -1,22 +1,72 @@
 // 1. Print numbers from 1 to 10
 // Use a for loop to print numbers from 1 to 10.
 
-for(let i=1; i<=10;i++){
-    console.log(i)
-}
+// for(let i=1; i<=10;i++){
+//     console.log(i)
+// }
 
 // 2. Print numbers from 10 to 1
 // Use a for loop to print numbers in reverse order.
 
-for (let i =10 ; i >= 1; i--){
-    console.log(i)
-}
+// for (let i =10 ; i >= 1; i--){
+//     console.log(i)
+// }
 
 // 3.Print even numbers
 // Print all even numbers between 1 and 20
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// 4. Print odd numbers
+// Print all odd numbers between 1 and 20.
+
+// for (let i = 1 ; i<=20; i+=2){
+//     console.log(i)
+// }
+
+
+
+//5. Print multiples of 5
+// Print the multiples of 5 from 5 to 50.
+
+// for (let i = 1 ; i<=50; i++){
+//     if(i % 5 === 0){
+//         console.log(i)
+//     }
+// }
+
+
+
+// 6.Print a message 10 times
+// Print "Hello JavaScript" 10 times using a for loop.
+
+
+// for (let a = 1 ; a<=20 ; a++){
+//     console.log("Hello JavaScript")
+// }
+
+
+
+// 7.Print numbers with their squares
+// Print the number and its square for numbers from 1 to 10.
+// Example:
+
+// for (let i =1 ; i <= 10; i++){
+//     console.log(i*i)
+// }
+
+
+// 8.Calculate the sum from 1 to 100
+// Use a for loop to calculate:
+
+let sum = 0
+for (let i = 1 ; i <=100 ; i++){
+    sum += i
 }
+
+console.log(sum)
