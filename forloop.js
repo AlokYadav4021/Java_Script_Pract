1c8b74f2318e26b15225c5cfac8c60d4718aa228
Alok Yadav <alokyadav4021@gmail.com>
2026-09-24 13:07:45 +0530

    10 More Intermediate Level practice question

--- forloop.js
+++ forloop.js
@@ -21,15 +21,12 @@
 //   }
 // }
 
-
 // 4. Print odd numbers
 // Print all odd numbers between 1 and 20.
 
 // for (let i = 1 ; i<=20; i+=2){
 //     console.log(i)
 // }
-
-
 
 //5. Print multiples of 5
 // Print the multiples of 5 from 5 to 50.
@@ -40,17 +37,12 @@
 //     }
 // }
 
-
-
 // 6.Print a message 10 times
 // Print "Hello JavaScript" 10 times using a for loop.
-
 
 // for (let a = 1 ; a<=20 ; a++){
 //     console.log("Hello JavaScript")
 // }
-
-
 
 // 7.Print numbers with their squares
 // Print the number and its square for numbers from 1 to 10.
@@ -60,13 +52,89 @@
 //     console.log(i*i)
 // }
 
-
 // 8.Calculate the sum from 1 to 100
 // Use a for loop to calculate:
 
-let sum = 0
-for (let i = 1 ; i <=100 ; i++){
-    sum += i
-}
+// let sum = 0
+// for (let i = 1 ; i <=100 ; i++){
+//     sum += i
+// }
 
-console.log(sum)
+// console.log(sum)
+
+// 9.Calculate the sum of even numbers
+// Find the sum of all even numbers between 1 and 100.
+
+// sum = 0 ;
+// for(let i = 0 ; i <= 100 ; i++){
+//     if(i % 2 === 0){
+//         sum+=i
+//     }
+// }
+// console.log(sum)
+
+// 10.Calculate the sum of odd numbers
+// Find the sum of all odd numbers between 1 and 100.
+
+// let sum = 0;
+// for (let i = 1; i <= 100 ; i+=2){
+//     sum += i
+// }
+// console.log(sum)
+
+// 11.Print a multiplication table
+// Create a variable:
+// let number = 7;
+
+// let number = 7;
+// for (let i = 1; i<=10; i++){
+// console.log(number * i);
+
+// }
+
+// 12.Create a variable number and calculate its factorial using a for loop.
+// Example:
+// 5! = 120
+
+// let fact = 1
+// for(let i = 1 ; i<=10;i++){
+//     fact*=i
+// }
+
+// console.log(fact)
+
+// 13.Count numbers divisible by 3
+// Count how many numbers between 1 and 100 are divisible by 3.
+
+// let count = 0;
+// for (let i = 1 ; i<= 100; i++){
+//     if(i%3===0){
+//         count++
+//     }
+// }
+// console.log(count)
+
+// 14.Find numbers divisible by both 3 and 5
+// Print all numbers between 1 and 100 that are divisible by both 3 and 5.
+
+// for (let i = 1 ; i<=100 ; i++){
+//     if (i%3===0 && i%5===0){
+//         console.log(i)
+//     }
+// }
+
+// 15.Calculate a power using a loop
+// Create:
+// let number = 2;
+// let power = 5;
+
+// let number = 2;
+// let power = 5;
+// let result = 1;
+
+// for (let i = 1; i <= power; i++) {
+//   result *= number;
+// }
+
+// console.log(result);
+
