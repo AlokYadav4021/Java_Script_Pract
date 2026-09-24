e1243b388f97c353df7f37b5fb86e630750af159
Alok Yadav <alokyadav4021@gmail.com>
2026-09-24 14:41:11 +0530

    strring method Beginner practice questions

---
+++ string-method.js
@@ -0,0 +1,59 @@
+// 1.Find the length of a string
+// Create let text = "JavaScript"; and print its length.
+
+// let text = "JavaScript";
+
+// console.log(text.length)
+
+// 2.Convert to uppercase
+// Convert "hello world" to uppercase using a string method.
+
+
+// let str = "Hello World"
+
+// console.log(str.toUpperCase())
+
+
+
+// 3.Convert to lowercase
+// Convert "JAVASCRIPT IS FUN" to lowercase.
+
+
+// let lower = "JAVASCRIPT IS FUN";
+// console.log(lower.toLowerCase())
+
+
+// 4.Find a character
+// Create let text = "JavaScript"; and use a string method to get the character at index
+
+
+// let text = "JavaScript";
+
+// console.log(text.charAt(4))
+
+
+// 5.Find the position of a word
+// Find the index of "world" in "Hello world".
+
+// let text = "Hello world";
+// console.log(text.indexOf("world"))
+
+// 6.Check if a string contains a word
+// Check whether "JavaScript is easy" contains "easy".
+
+
+// let text = "JavaScript is easy";
+// console.log(text.includes("easy"))
+
+
+// 7.Check the beginning of a string
+// Check whether "Hello JavaScript" starts with "Hello"
+
+// let txt = "Hello JavaScript";
+// console.log(txt.startsWith("Hello"))
+
+// 8.Check the ending of a string
+// Check whether "myfile.jpg" ends with ".jpg".
+
+// let txt = "myfile.jpg";
+// console.log(txt.endsWith("jpg"))
