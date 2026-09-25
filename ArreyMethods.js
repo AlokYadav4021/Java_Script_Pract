3950ca2512ca32fbb4cb91e161ff1f7a99dd627b
Alok Yadav <alokyadav4021@gmail.com>
2026-09-26 00:46:41 +0530

    Arrey Beginers Practice Questions

---
+++ ArreyMethods.js
@@ -0,0 +1,74 @@
+// 1.Create and print an array
+// Create an array containing 5 fruits and print the array.
+
+// let arr = ["Mango", "Banana","Apple","Pineapple","Lichi"]
+// console.log(arr)
+
+// 2.Access an array element
+// Create an array of 5 colors. Print the first and last elements.
+
+// let arr = ["Red" , "Green" , "Blue", "Pink", "Skyblue" ]
+// console.log(arr[0]);
+// console.log(arr[arr.length - 1])
+
+
+// 3.Change an array element
+// Create:
+
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// fruits[2] = "Chikuu"
+// console.log(fruits)
+
+
+// 4.Change an array element
+// Create:
+
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// Change "Banana" to "Grapes" and print the array.
+
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// console.log(fruits)
+// fruits[1] = "Grapes";
+
+// 5.Find array length
+// Create an array of 7 numbers and print its length.
+
+// let arr = [11,22,21,22,45,56,47]
+// console.log(arr.length)
+
+
+// 6.Add an element using push()
+// Create an array of 4 animals. Add "Lion" to the end using push().
+
+// let ani = ["Tiger", "Deer" , "Dog" , "Cat"]
+// let a = ani.push("Lion")
+// console.log(ani)
+
+// 7.Remove an element using pop()
+// Create an array of 5 numbers. Remove the last element using pop() and print the result.
+
+
+// let num = [12,33,45,78,234]
+// let a = num.pop()
+// console.log(num)
+
+// 8.Add an element using unshift()
+// Create an array of 4 names. Add a new name to the beginning using unshift().
+
+// let names = ["Alok","Ravi", "Karan", "Vishal"]
+// let nname = names.unshift("Viraj")
+// console.log(names)
+
+
+// 9.Remove an element using shift()
+// Create an array of 5 cities. Remove the first city using shift().
+
+// let cities = ["Pune","Banglore","Mumbai","Chennai","Delhi"]
+
+// let sit = cities.shift()
+// console.log(cities)
