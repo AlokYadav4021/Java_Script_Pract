c22f3dd962a96cea8663ed2f58878881836a62e9
Alok Yadav <alokyadav4021@gmail.com>
2026-09-26 01:46:59 +0530

    arrey Intermediate practice Questions

--- ArreyMethods.js
+++ ArreyMethods.js
@@ -11,7 +11,6 @@
 // console.log(arr[0]);
 // console.log(arr[arr.length - 1])
 
-
 // 3.Change an array element
 // Create:
 
@@ -21,7 +20,6 @@
 
 // fruits[2] = "Chikuu"
 // console.log(fruits)
-
 
 // 4.Change an array element
 // Create:
@@ -41,7 +39,6 @@
 // let arr = [11,22,21,22,45,56,47]
 // console.log(arr.length)
 
-
 // 6.Add an element using push()
 // Create an array of 4 animals. Add "Lion" to the end using push().
 
@@ -51,7 +48,6 @@
 
 // 7.Remove an element using pop()
 // Create an array of 5 numbers. Remove the last element using pop() and print the result.
-
 
 // let num = [12,33,45,78,234]
 // let a = num.pop()
@@ -64,7 +60,6 @@
 // let nname = names.unshift("Viraj")
 // console.log(names)
 
-
 // 9.Remove an element using shift()
 // Create an array of 5 cities. Remove the first city using shift().
 
@@ -72,3 +67,94 @@
 
 // let sit = cities.shift()
 // console.log(cities)
+
+// 10.Find an element using indexOf()
+// Create:
+
+// let fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+// Find the index of "Banana".
+
+// let fruits = ["Apple", "Mango", "Banana", "Orange"];
+// console.log(fruits.indexOf("Banana"));
+
+// 11.Check an element using includes()
+// Create an array of programming languages. Check whether "JavaScript" exists in the array.
+
+// let plang = ["HTML","CSS","JS","Python"]
+// console.log(plang.includes("JS"))
+
+// 12.Print all elements using a for loop
+// Create an array of 5 numbers and print every element using a for loop.
+
+// let plang = ["HTML","CSS","JS","Python"]
+
+// for(let lang of plang){
+//     console.log(lang)
+// }
+
+// 13.Calculate the sum of array elements
+// Create:
+// let numbers = [10, 20, 30, 40, 50];
+// Calculate and print the total sum using a loop.
+
+// let numbers = [10, 20, 30, 40, 50];
+// let cont = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   cont += numbers[i];
+// }
+
+// console.log(cont);
+
+
+// 14.Find the largest number
+// Create:
+// let numbers = [12, 45, 7, 89, 23, 56];
+// Find the largest number using a loop.
+
+// let number = [12, 45, 7, 89, 23, 56];
+// let lrg = number[0]
+
+// for (let i = 0 ; i < number.length; i++){
+//     if( number[i] > lrg){
+//         lrg = number[i]
+//     }
+// }
+
+// console.log(lrg)
+
+// 15.Find the smallest number
+// Create:
+// let numbers = [12, 45, 7, 89, 23, 56];
+// Find the smallest number using a loop.
+
+// let numbers = [12, 45, 7, 89, 23, 56];
+
+// let sml = numbers[0];
+
+// for (let i = 0 ; i< numbers.length ; i++){
+//     if (numbers[i] < sml){
+//         sml = numbers[i]
+//     }
+// }
+
+// console.log(sml)
+
+
+// 16.Count even numbers
+// Create an array of numbers and count how many numbers are even.
+
+// let num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+
+// let cont = 0
+
+// for (let i = 0 ; i < num.length; i++){
+//     if (num[i] % 2 === 0){
+//         cont ++
+//     }
+// }
+
+// console.log(cont)
+
+
